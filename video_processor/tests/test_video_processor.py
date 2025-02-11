68ce32aa208d76fa0ed9e1985d3e962fdfbf7b18
Егор Белов <e1belov@yandex.ru>
2025-02-11 19:17:41 +0500

    done x7

--- video_processor/tests/test_video_processor.py
+++ video_processor/tests/test_video_processor.py
@@ -2,7 +2,7 @@ import requests
 
 def test_video_processor():
     url = 'http://video_processor:5000/process' 
-    files = {'file': open('test_video.mp4', 'rb')}
+    files = {'file': open('/app/tests/test.mp4', 'rb')}  # Путь к файлу внутри контейнера
     response = requests.post(url, files=files)
     assert response.status_code == 200
     assert "objects" in response.json()
--- video_searcher/tests/test_video_searcher.py
+++ video_searcher/tests/test_video_searcher.py
@@ -2,7 +2,7 @@ import requests
 
 def test_video_searcher():
     url = 'http://video_searcher:5001/search'  
-    files = {'file': open('test_video.mp4', 'rb')}
+    files = {'file': open('/app/tests/test.mp4', 'rb')}  # Путь к файлу внутри контейнера
     response = requests.post(url, files=files)
     assert response.status_code == 200
     assert "filtered_objects" in response.json()
